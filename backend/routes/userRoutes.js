2880278ce2fc25a892f084790c74e7bcafb81baf
devanshu <devanshuchuhan210502@gmail.com>
2024-07-14 22:35:22 +0530

    first commit

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,10 @@
+import express from "express";
+import { createUser,loginUser,logoutCurrentUser } from "../controllers/userController.js";
+
+const router = express.Router();
+
+router.route("/").post(createUser);
+router.post('/auth',loginUser)
+router.post('/logout',logoutCurrentUser)
+
+export default router;
