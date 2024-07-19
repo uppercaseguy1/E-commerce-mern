3688d617438271f26364ed4a948e96a073870501
devanshu <devanshuchuhan210502@gmail.com>
2024-07-19 12:19:38 +0530

    category-backend-added

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ import cookieParser from "cookie-parser";
 // Utiles
 import connectDB from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
-// import categoryRoutes from "./routes/categoryRoutes.js";
+import categoryRoutes from "./routes/categoryRoutes.js";
 // import productRoutes from "./routes/productRoutes.js";
 // import uploadRoutes from "./routes/uploadRoutes.js";
 // import orderRoutes from "./routes/orderRoutes.js";
@@ -24,7 +24,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use("/api/users", userRoutes);
-// app.use("/api/category", categoryRoutes);
+app.use("/api/category", categoryRoutes);
 // app.use("/api/products", productRoutes);
 // app.use("/api/upload", uploadRoutes);
 // app.use("/api/orders", orderRoutes);
