1339c98e9fdec8d06c008e7d5254aafe3f1d7371
devanshu <devanshuchuhan210502@gmail.com>
2024-07-29 22:13:46 +0530

    cors-added

--- backend/index.js
+++ backend/index.js
@@ -12,12 +12,16 @@ import productRoutes from "./routes/productRoutes.js";
 import uploadRoutes from "./routes/uploadRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
 
+
 dotenv.config();
 const port = process.env.PORT || 5000;
 
 connectDB();
 
 const app = express();
+
+const cors = require("cors");
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
--- frontend/src/redux/constants.js
+++ frontend/src/redux/constants.js
@@ -1,4 +1,5 @@
 export const BASE_URL = "https://e-commerce-mern-2qsa.onrender.com";
+// export const BASE_URL = "";
 export const USERS_URL = "/api/users";
 export const CATEGORY_URL = "/api/category";
 export const PRODUCT_URL = "/api/products";
