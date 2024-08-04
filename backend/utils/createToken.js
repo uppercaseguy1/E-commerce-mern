444d7b6d68305a92bf667fe79ceb0b8a6056552a
devanshu <devanshuchuhan210502@gmail.com>
2024-08-04 15:13:06 +0530

    ..

--- backend/utils/createToken.js
+++ backend/utils/createToken.js
@@ -1,11 +1,46 @@
+// import jwt from "jsonwebtoken";
+
+// const generateToken = (res, userId) => {
+//     const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+//         expiresIn: "30d",
+//     });
+
+//     // Set JWT as an HTTP-Only Cookie
+//     res.cookie("jwt", token, {
+//         httpOnly: true,
+//         secure: process.env.NODE_ENV === "production",
+//         sameSite: "strict",
+//         maxAge: 30 * 24 * 60 * 60 * 1000,
+//     });
+
+//     return token;
+// };
+
+// export default generateToken;
+
+//
+
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 import jwt from "jsonwebtoken";
+
+const app = express();
+
+const corsOptions = {
+    origin: 'https://nexcommerce.netlify.app',
+    credentials: true,
+};
+
+app.use(cors(corsOptions));
+app.use(cookieParser());
+app.use(express.json());
 
 const generateToken = (res, userId) => {
     const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
         expiresIn: "30d",
     });
 
-    // Set JWT as an HTTP-Only Cookie
     res.cookie("jwt", token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
@@ -17,39 +52,3 @@ const generateToken = (res, userId) => {
 };
 
 export default generateToken;
-
-//
-
-// import express from "express";
-// import cookieParser from "cookie-parser";
-// import cors from "cors";
-// import jwt from "jsonwebtoken";
-
-// const app = express();
-
-// const corsOptions = {
-//     origin: 'https://nexcommerce.netlify.app',
-//     credentials: true,
-// };
-
-// app.use(cors(corsOptions));
-// app.use(cookieParser());
-// app.use(express.json());
-
-// const generateToken = (res, userId) => {
-//     const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-//         expiresIn: "30d",
-//     });
-
-//     res.cookie("jwt", token, {
-//         httpOnly: true,
-//         secure: process.env.NODE_ENV === "production",
-//         sameSite: "strict",
-//         maxAge: 30 * 24 * 60 * 60 * 1000,
-//         domain: '.netlify.app', // Adjust according to your domain
-//     });
-
-//     return token;
-// };
-
-// export default generateToken;
