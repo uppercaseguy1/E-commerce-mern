f9607321e84c5912c9f49b93d81a9e5bb6ca0ab1
devanshu <devanshuchuhan210502@gmail.com>
2024-08-05 17:09:00 +0530

    ...

--- backend/utils/createToken.js
+++ backend/utils/createToken.js
@@ -3,12 +3,13 @@ import jwt from "jsonwebtoken";
 const generateToken = (res, userId) => {
     const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
         expiresIn: "30d",
+        secure:true
     });
 
     // Set JWT as an HTTP-Only Cookie
     res.cookie("jwt", token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV !== "production",
+        secure: process.env.NODE_ENV === "production",
         sameSite: "strict",
         maxAge: 30 * 24 * 60 * 60 * 1000,
     });
