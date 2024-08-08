15742dc2d30f5dd8b78a64448a19e85c5cd994ac
devanshu <devanshuchuhan210502@gmail.com>
2024-08-08 12:37:45 +0530

    ..

--- backend/utils/createToken.js
+++ backend/utils/createToken.js
@@ -6,12 +6,12 @@ const generateToken = (res, userId) => {
     });
 
     // Set JWT as an HTTP-Only Cookie
-    res.cookie("jwt", token, {
-        httpOnly: true,
-        // secure: process.env.NODE_ENV === "production",
-        sameSite: "strict",
-        maxAge: 30 * 24 * 60 * 60 * 1000,
-    });
+    // res.cookie("jwt", token, {
+    //     httpOnly: true,
+    //     // secure: process.env.NODE_ENV === "production",
+    //     sameSite: "strict",
+    //     maxAge: 30 * 24 * 60 * 60 * 1000,
+    // });
 
     return token;
 };
