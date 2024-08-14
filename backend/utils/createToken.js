b6472fd9776d7ce1ba082d05bb62dfe78811142d
devanshu <devanshuchuhan210502@gmail.com>
2024-08-14 11:44:39 +0530

    ..

--- backend/utils/createToken.js
+++ backend/utils/createToken.js
@@ -8,7 +8,8 @@ const generateToken = (res, userId) => {
     // Set JWT as an HTTP-Only Cookie
     res.cookie("jwt", token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
+        // secure: process.env.NODE_ENV === "production",
+        secure: false,
         sameSite: "strict",
         maxAge: 30 * 24 * 60 * 60 * 1000,
     });
