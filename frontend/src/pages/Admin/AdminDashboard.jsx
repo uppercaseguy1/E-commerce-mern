c432a5f2e4185a26e33c511857472f2432aa5b44
devanshu <devanshuchuhan210502@gmail.com>
2025-01-05 12:53:35 +0530

    ..

--- frontend/src/pages/Admin/AdminDashboard.jsx
+++ frontend/src/pages/Admin/AdminDashboard.jsx
@@ -111,7 +111,7 @@ const AdminDashboard = () => {
 
                         <p className="mt-5">Customers</p>
                         <h1 className="text-xl font-bold">
-                            $ {isLoading ? <Loader /> : customers?.length}
+                            {isLoading ? <Loader /> : customers?.length}
                         </h1>
                     </div>
                     <div className="rounded-lg bg-black p-5 w-[20rem] mt-5">
@@ -121,7 +121,7 @@ const AdminDashboard = () => {
 
                         <p className="mt-5">All Orders</p>
                         <h1 className="text-xl font-bold">
-                            $ {isLoading ? <Loader /> : orders?.totalOrders}
+                            {isLoading ? <Loader /> : orders?.totalOrders}
                         </h1>
                     </div>
                 </div>
